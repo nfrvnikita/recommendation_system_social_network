5cff583ed671e01d80a41a460fc248f8817cc1df
Никита Ануфриев <nv.anufriev@Nikitas-MacBook-Pro.local>
2023-02-08 00:09:36 +0300

    add a function load_features.py

--- load_features.py
+++ load_features.py
@@ -2,8 +2,9 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 engine = create_engine(
-    "postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
-    "postgres.lab.karpov.courses:6432/startml")
+        "postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
+        "postgres.lab.karpov.courses:6432/startml"
+    )
 
 
 def batch_load_sql(query: str) -> pd.DataFrame:
@@ -21,11 +22,4 @@ def batch_load_sql(query: str) -> pd.DataFrame:
 
 
 def load_features() -> pd.DataFrame:
-
-    user_df = pd.read_sql('SELECT * FROM nfrv_nikita_features_lesson_22', con=engine)
-    post_df = pd.read_sql('SELECT * FROM nfrv_nikita_features_lesson_22', con=engine)
-
-    user_data = batch_load_sql(user_df)
-    post_data = batch_load_sql(post_df)
-
-    return [user_data, post_data]
+    return batch_load_sql('SELECT * FROM nfrv_nikita_features_lesson_22')
